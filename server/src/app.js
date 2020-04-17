1b66021d3a8a629dfb9198f044a730a8ee18689e
moadij <moalstigsson@gmail.com>
2020-04-16 23:38:26 -0600

    added backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,21 @@
-console.log('hello');
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `hello ${req.body.email} ! your user was registered!`
-    })
-})
+require('./passport')
 
-app.listen(process.env.PORT || 8081)
+require('./routes')(app)
+
+sequelize.sync({force: false})
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
