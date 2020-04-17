f0fcf479b6eb9650ad72a57a5f5f52300f30400f
moadij <moalstigsson@gmail.com>
2020-04-16 23:25:24 -0600

    added front end

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '@/components/Register'
+import Login from '@/components/Login'
+import Songs from '@/components/Songs/Index'
+import CreateSong from '@/components/CreateSong'
+import EditSong from '@/components/EditSong'
+import ViewSong from '@/components/ViewSong/Index'
 
 Vue.use(Router)
 
@@ -11,5 +16,34 @@ export default new Router({
       name: 'register',
       component: Register
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/songs',
+      name: 'songs',
+      component: Songs
+    },
+    {
+      path: '/songs/create',
+      name: 'songs-create',
+      component: CreateSong
+    },
+    {
+      path: '/songs/:songId',
+      name: 'song',
+      component: ViewSong
+    },
+    {
+      path: '/songs/:songId/edit',
+      name: 'song-edit',
+      component: EditSong
+    },
+    {
+      path: '*',
+      redirect: 'songs'
+    }
   ]
 })
